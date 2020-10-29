3d65df016d3d8bea446c6d8dcadeb34756c3b4b5
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-29 17:17:18 -0500

    used shortid instead of assignID to generate random id and fixed the bug of not being able to delete first note

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,37 +1,33 @@
-const savedNotes = require("../db/db.json");
+let savedNotes = require("../db/db.json");
 const fs = require("fs");
+const shortid = require("shortid");
 
-module.exports = function(app){
-    app.get("/api/notes", function(req,res){
-      return res.json(savedNotes)
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        return res.json(savedNotes);
     });
-    app.post("/api/notes", function(req,res){
-        let newNote = req.body;
+    app.post("/api/notes", function (req, res) {
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: shortid.generate()
+        };
         savedNotes.push(newNote);
-        assignID();
-        fs.writeFile("./db/db.json", JSON.stringify(savedNotes),function(err) {
+        fs.writeFile("./db/db.json", JSON.stringify(savedNotes), function (err) {
             if (err) throw err;
         });
         res.json(savedNotes);
         console.log(savedNotes);
-    }); 
+    });
 
-    app.delete("/api/notes/:id", function(req,res){
-       const deletedID = req.params.id;
-       console.log(deletedID);
-       const deletedNote = savedNotes.splice(deletedID,1);
-       console.log(savedNotes);
-       assignID();
-       fs.writeFile("./db/db.json", JSON.stringify(savedNotes), function (err) {
-        if (err)  throw err
+    app.delete("/api/notes/:id", function (req, res) {
+        const deletedID = req.params.id;
+        console.log(deletedID);
+        savedNotes.splice(deletedID, 1);
+        console.log(savedNotes);
+        fs.writeFile("./db/db.json", JSON.stringify(savedNotes), function (err) {
+            if (err) throw err
         });
         res.json(savedNotes);
-        })
-        
-    
-    function assignID() {
-        for (i = 0; i < savedNotes.length; i ++) {
-            savedNotes[i].id = i;
-        }
-    }       
+    })     
 }
